036b1397d531d068812e7eddccb0cb35c2dd45b5
kalinga890 <kalingaa69@gmail.com>
2025-09-26 09:12:13 +0530

    primary annotation concept

---
+++ primaryAnnotation/src/test/java/com/example/primaryAnnotation/PrimaryAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primaryAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
